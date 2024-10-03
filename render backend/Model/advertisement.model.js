34552141b68ae549adf84514353a137b42b9db4e
Kishore Selvaraj <89259172+Kishoreselvaraj@users.noreply.github.com>
2024-10-03 12:24:54 +0530

    remove

--- render backend/Model/advertisement.model.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require("mongoose");
-
-const advertisementSchema = new Schema({
-  image: {
-    type: String,
-  },
-});
-
-module.exports = model("Advertisement", advertisementSchema);
