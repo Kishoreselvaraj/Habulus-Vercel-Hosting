369399e691c2f5c45a49a1fa58c63937bca58fb0
Kishore Selvaraj <89259172+Kishoreselvaraj@users.noreply.github.com>
2024-10-03 10:31:33 +0530

    env change

--- Website/src/utils/api.js
+++ Website/src/utils/api.js
@@ -1,2 +1,2 @@
 export const API_LINK = "https://backend-habulus.onrender.com"+ "/api/v1"
-export const MEDIA_LINK = import.meta.env.VITE_API_LINK + "/media/"
+export const MEDIA_LINK = "https://backend-habulus.onrender.com" + "/media/"
